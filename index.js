7a131f9004692591cc972b2a1a3454c847ee5aa4
annabessant <anna@thefutureofinnovation.org>
2023-02-27 10:52:21 +0000

    add code to initialize the app

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
+const generateHTML = require(`./src/generateHTML`);
+let answersArr = [];
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
@@ -30,7 +32,7 @@ const manager = [
     {
         type: 'input',
         name: 'name',
-        message: "What is Manager's first name?",
+        message: "What is Manager's full name?",
     },
     {
         type: 'number',
@@ -55,7 +57,7 @@ const engineer = [
     {
         type: 'input',
         name: 'name',
-        message: "What is Engineer's first name?",
+        message: "What is Engineer's full name?",
     },
     {
         type: 'number',
@@ -80,7 +82,7 @@ const intern = [
     {
         type: 'input',
         name: 'name',
-        message: "What is Intern's first name?",
+        message: "What is Intern's full name?",
     },
     {
         type: 'number',
@@ -98,3 +100,48 @@ const intern = [
         message: "What school does Intern go?",
     },
 ];
+
+// initialize the app
+function init () {
+    // prompts 
+    inquirer.prompt(questions).then(answers => {
+        // if the task is comlete and you are DONE
+        if (answers.role === 'DONE') {
+            console.log(answersArr);
+            generateTeam();
+            return;
+        }
+
+        // Adding manager 
+        if (answers.role === 'Manager') {
+            inquirer.prompt(manager).then(answers => {
+                const manager = new Manager(answers.name, answers.id, answers.email, answers.office);
+            answersArr.push(manager);
+            init();
+            })
+        }
+        // Adding engineer
+        if (answers.role === 'Engineer') {
+            inquirer.prompt(engineer).then(answers => {
+                const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+            answersArr.push(engineer);
+            init();
+            })
+        }
+        // Adding intern
+        if (answers.role === 'Intern') {
+            inquirer.prompt(intern).then(answers => {
+                    const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+            answersArr.push(intern);
+            init();
+            })
+        }
+    })
+};
+init();
+
+// new members
+function generateTeam() {
+    fs.writeFileSync(`./dist/generatedTeam.html`, generateHTML(answersArr), "utf-8");
+    console.log('My Team')
+};
